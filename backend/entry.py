04cf56478ce77f3bf74934935071dfa12db22c6a
bryanmwaniqi <bryanmwaniqi@gmail.com>
2024-08-20 16:29:44 +0300

    setup boilerplate for API methods

--- backend/entry.py
+++ backend/entry.py
@@ -6,16 +6,24 @@ app = Flask(__name__)
 api = Api(app)
 
 class CalendarEvent(Resource):
+    # Fetch event
     def get(self):
         resp = jsonify({'status-testing': 'ok'})
         return resp
+    
+    # Add event
     def post(self):
-        pass
-    def put(self):
-        pass
-    def delete(self):
-        pass
+        data = request.get_json()
 
+    # Update event
+    def put(self):
+        data = request.get_json()
+
+    # Delete event
+    def delete(self):
+        data = request.get_json()
+        
 api.add_resource(CalendarEvent, '/event')
+
 if __name__ == '__main__':
     app.run()
